2cd47fac1b571c7e2bcb26988e71136ebe70163c
Abbas Elmas <abbaselmas@gmail.com>
2022-04-15 17:16:15 +0300

    typo

--- user_data/strategies/abbas.py
+++ user_data/strategies/abbas.py
@@ -407,7 +407,7 @@ class abbas(IStrategy):
         dataframe['sma_9'] = ta.SMA(dataframe, timeperiod=9)
         
         # Elliot
-        dataframe['EWO'] = EWO(dataframe, self.fast_ewo.value, self.slow_ewo.value)
+        dataframe['EWO'] = EWO(dataframe, self.fast_ewo, self.slow_ewo)
 
         # RSI
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
