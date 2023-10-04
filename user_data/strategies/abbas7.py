c488cc3c3dd046edeb83ed978fb27c44b4d9f6aa
Abbas Elmas <abbaselmas@gmail.com>
2023-10-04 14:54:11 +0300

    organize things, func ewo problem

--- user_data/strategies/abbas7.py
+++ user_data/strategies/abbas7.py
@@ -15,7 +15,6 @@ import technical.indicators as ftt
 from freqtrade.exchange import timeframe_to_prev_date
 from freqtrade.optimize.space import Categorical, Dimension, Integer, SKDecimal, Real
 
-
 # Protection hyperspace params:
 protection_params = {
     "cooldown_stop_duration_candles": 4,
@@ -31,7 +30,6 @@ protection_params = {
     "stoplossguard_stop_duration_candles": 9,
     "stoplossguard_trade_limit": 18
 }
-
 # Buy hyperspace params:
 buy_params = {
     "base_nb_candles_buy": 30,
@@ -41,27 +39,18 @@ buy_params = {
     "low_offset": 1.083,
     "low_offset_2": 0.942,
     "min_profit": 0.74,
-    "rsi_buy": 60
+    "rsi_buy": 60,
+    "fast_ewo": 50,
+    "slow_ewo": 200
 }
-
 # Sell hyperspace params:
 sell_params = {
     "base_nb_candles_sell": 8,
     "high_offset": 1.002,
-    "high_offset_ema": 1.063,
-    "fast_ewo": 50,
-    "slow_ewo": 200
+    "high_offset_ema": 1.063
 }
 
-def EWO(dataframe, ema_length=5, ema2_length=35):
-    df = dataframe.copy()
-    ema1 = ta.EMA(df, timeperiod=ema_length)
-    ema2 = ta.EMA(df, timeperiod=ema2_length)
-    emadif = (ema1 - ema2) / df["low"] * 100
-    return emadif
-
 class abbas7(IStrategy):
-
     INTERFACE_VERSION = 2
 
     cooldown_stop_duration_candles = IntParameter(0, 15, default=protection_params["cooldown_stop_duration_candles"], space="protection", optimize=True)
@@ -129,98 +118,66 @@ class abbas7(IStrategy):
                 Categorical([True], name="trailing_only_offset_is_reached"),
             ]
 
-    # ROI table:
-    minimal_roi = {
-        "200": 0
-    }
-
-    # Stoploss:
-    stoploss = -0.094
-
-    # Trailing stop:
-    trailing_stop = True
-    trailing_stop_positive = 0.0001
-    trailing_stop_positive_offset = 0.0145
-    trailing_only_offset_is_reached = True
-
     # Sell signal
     use_sell_signal = False
     ignore_roi_if_buy_signal = False
 
-    # SMAOffset
-    smaoffset_optimize = False
-    high_offset_ema = DecimalParameter(0.90, 1.1, default=sell_params["high_offset_ema"], load=True, space="sell", decimals=3, optimize=smaoffset_optimize)
-    base_nb_candles_buy = IntParameter(15, 30, default=buy_params["base_nb_candles_buy"], space="buy", optimize=smaoffset_optimize)
-    base_nb_candles_sell = IntParameter(5, 30, default=sell_params["base_nb_candles_sell"], space="sell", optimize=smaoffset_optimize)
-    low_offset = DecimalParameter(1.0, 1.1, default=buy_params["low_offset"], space="buy", decimals=3, optimize=smaoffset_optimize)
-    low_offset_2 = DecimalParameter(0.94, 0.98, default=buy_params["low_offset_2"], space="buy", decimals=3, optimize=smaoffset_optimize)
-    high_offset = DecimalParameter(1.0, 1.1, default=sell_params["high_offset"], space="sell", decimals=3, optimize=smaoffset_optimize)
+    sell_optimize = True
+    high_offset_ema = DecimalParameter(0.90, 1.1, default=sell_params["high_offset_ema"], load=True, space="sell", decimals=3, optimize=sell_optimize)
+    base_nb_candles_sell = IntParameter(5, 30, default=sell_params["base_nb_candles_sell"], space="sell", optimize=sell_optimize)
+    high_offset = DecimalParameter(1.0, 1.1, default=sell_params["high_offset"], space="sell", decimals=3, optimize=sell_optimize)
 
-    # Protection
-    fast_ewo = IntParameter(5,60, default=sell_params["fast_ewo"], space="sell", optimize=True)
-    slow_ewo = IntParameter(80,300, default=sell_params["slow_ewo"], space="sell", optimize=True)
-
-    protection_optimize = True
-    ewo_low = DecimalParameter(-13.0, -9.0,default=buy_params["ewo_low"], space="buy", decimals=2, optimize=protection_optimize)
-    ewo_high = DecimalParameter(1.2, 2.4, default=buy_params["ewo_high"], space="buy", decimals=3, optimize=protection_optimize)
-    ewo_high_2 = DecimalParameter(-4.0, -1.6, default=buy_params["ewo_high_2"], space="buy", decimals=2, optimize=protection_optimize)
-    rsi_buy = IntParameter(55, 85, default=buy_params["rsi_buy"], space="buy", optimize=protection_optimize)
-    min_profit = DecimalParameter(0.60, 1.00, default=buy_params["min_profit"], space="buy", decimals=2, optimize=protection_optimize)
-
-    # Optional order time in force.
-    order_time_in_force = {
-        "entry": "gtc",
-        "exit": "ioc"
-    }
-
-    # Optimal timeframe for the strategy
-    timeframe = "5m"
+    buy_optimize= True
+    base_nb_candles_buy = IntParameter(15, 30, default=buy_params["base_nb_candles_buy"], space="buy", optimize=buy_optimize)
+    ewo_high = DecimalParameter(1.2, 2.4, default=buy_params["ewo_high"], space="buy", decimals=3, optimize=buy_optimize)
+    ewo_high_2 = DecimalParameter(-4.0, -1.6, default=buy_params["ewo_high_2"], space="buy", decimals=2, optimize=buy_optimize)
+    ewo_low = DecimalParameter(-13.0, -9.0,default=buy_params["ewo_low"], space="buy", decimals=2, optimize=buy_optimize)
+    low_offset = DecimalParameter(1.0, 1.1, default=buy_params["low_offset"], space="buy", decimals=3, optimize=buy_optimize)
+    low_offset_2 = DecimalParameter(0.94, 0.98, default=buy_params["low_offset_2"], space="buy", decimals=3, optimize=buy_optimize)
+    min_profit = DecimalParameter(0.60, 1.00, default=buy_params["min_profit"], space="buy", decimals=2, optimize=buy_optimize)
+    rsi_buy = IntParameter(55, 85, default=buy_params["rsi_buy"], space="buy", optimize=buy_optimize)
+    
+    ewo_optimize = True
+    fast_ewo = IntParameter(5,60, default=sell_params["fast_ewo"], space="ewo", optimize=ewo_optimize)
+    slow_ewo = IntParameter(80,300, default=sell_params["slow_ewo"], space="ewo", optimize=ewo_optimize)
+    
     inf_1h = "1h"
 
     process_only_new_candles = True
     startup_candle_count = 200
 
-    slippage_protection = {
-        "retries": 3,
-        "max_slippage": -0.002
-    }
-
-    buy_signals = {}
+    def EWO(self, dataframe, ema_length=5, ema2_length=35):
+        df = dataframe.copy()
+        ema1 = ta.EMA(df, timeperiod=ema_length)
+        ema2 = ta.EMA(df, timeperiod=ema2_length)
+        emadif = (ema1 - ema2) / df["low"] * 100
+        return emadif
 
     def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float, rate: float, time_in_force: str, sell_reason: str, current_time: datetime, **kwargs) -> bool:
-
         dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-
         # slippage
         try:
             state = self.slippage_protection["__pair_retries"]
         except KeyError:
             state = self.slippage_protection["__pair_retries"] = {}
-
         candle = dataframe.iloc[-1].squeeze()
-
         slippage = (rate / candle["close"]) - 1
         if slippage < self.slippage_protection["max_slippage"]:
             pair_retries = state.get(pair, 0)
             if pair_retries < self.slippage_protection["retries"]:
                 state[pair] = pair_retries + 1
                 return False
-
         state[pair] = 0
         return True
 
     def informative_pairs(self):
-        # get access to all pairs available in whitelist.
-        pairs = self.dp.current_whitelist()
-        # Assign tf to each pair so they can be downloaded and cached for strategy.
-        informative_pairs = [(pair, "1h") for pair in pairs]
+        pairs = self.dp.current_whitelist() # get access to all pairs available in whitelist.
+        informative_pairs = [(pair, "1h") for pair in pairs] # Assign tf to each pair so they can be downloaded and cached for strategy.
         return informative_pairs
 
     def informative_1h_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         assert self.dp, "DataProvider is required for multiple timeframes."
-        # Get the informative pair
         informative_1h = self.dp.get_pair_dataframe(pair=metadata["pair"], timeframe=self.inf_1h)
-
         informative_1h["hma_50"] = qtpylib.hull_moving_average(informative_1h["close"], window=50)
         informative_1h["ema_100"] = ta.EMA(informative_1h, timeperiod=100)
         informative_1h["ema_12"] = ta.EMA(informative_1h, timeperiod=12)
@@ -231,31 +188,21 @@ class abbas7(IStrategy):
         informative_1h["sma_200"] = ta.SMA(informative_1h, timeperiod=200)
         informative_1h["sma_200_dec"] = informative_1h["sma_200"] < informative_1h["sma_200"].shift(20)
         informative_1h["sma_9"] = ta.SMA(informative_1h, timeperiod=9)
-
-        # Elliot
-        fast_ew = self.fast_ewo.value
-        slow_ew = self.slow_ewo.value
-        informative_1h["EWO"] = EWO(informative_1h, fast_ew, slow_ew)
-
-        # RSI
+        informative_1h["EWO"] = EWO(informative_1h, self.fast_ewo.value, self.slow_ewo.value)
         informative_1h["rsi"] = ta.RSI(informative_1h, timeperiod=14)
         informative_1h["rsi_fast"] = ta.RSI(informative_1h, timeperiod=4)
         informative_1h["rsi_slow"] = ta.RSI(informative_1h, timeperiod=20)
-
         return informative_1h
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
         # Calculate all ma_buy values
         for val in self.base_nb_candles_buy.range:
             dataframe[f"ma_buy_{val}"] = ta.EMA(dataframe, timeperiod=val)
-
         # Calculate all ma_sell values
         for val in self.base_nb_candles_sell.range:
             dataframe[f"ma_sell_{val}"] = ta.EMA(dataframe, timeperiod=val)
 
         dataframe["hma_50"] = qtpylib.hull_moving_average(dataframe["close"], window=50)
-
         dataframe["ema_100"] = ta.EMA(dataframe, timeperiod=100)
         dataframe["ema_12"] = ta.EMA(dataframe, timeperiod=12)
         dataframe["ema_20"] = ta.EMA(dataframe, timeperiod=20)
@@ -265,24 +212,15 @@ class abbas7(IStrategy):
         dataframe["sma_200"] = ta.SMA(dataframe, timeperiod=200)
         dataframe["sma_200_dec"] = dataframe["sma_200"] < dataframe["sma_200"].shift(20)
         dataframe["sma_9"] = ta.SMA(dataframe, timeperiod=9)
-
-        # Elliot
-        fast_ew = self.fast_ewo.value
-        slow_ew = self.slow_ewo.value
-        dataframe["EWO"] = EWO(dataframe, fast_ew, slow_ew)
-
-        # RSI
+        dataframe["EWO"] = self.EWO(dataframe, self.fast_ewo.value, self.slow_ewo.value)
         dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
         dataframe["rsi_fast"] = ta.RSI(dataframe, timeperiod=4)
         dataframe["rsi_slow"] = ta.RSI(dataframe, timeperiod=20)
-
         informative_1h = self.informative_1h_indicators(dataframe, metadata)
         dataframe = merge_informative_pair(dataframe, informative_1h, self.timeframe, self.inf_1h, ffill=True)
-
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
         dataframe.loc[
             (
                 (dataframe["rsi_fast"] < 35) &
@@ -293,7 +231,6 @@ class abbas7(IStrategy):
                 (dataframe["close"] < (dataframe[f"ma_sell_{self.base_nb_candles_sell.value}"] * self.high_offset.value))
             ),
             ["buy", "buy_tag"]] = (1, "ewo1")
-
         dataframe.loc[
             (
                 (dataframe["rsi_fast"] < 35) &
@@ -305,7 +242,6 @@ class abbas7(IStrategy):
                 (dataframe["rsi"] < 25)
             ),
             ["buy", "buy_tag"]] = (1, "ewo2")
-
         dataframe.loc[
             (
                 (dataframe["rsi_fast"] < 35) &
@@ -315,19 +251,15 @@ class abbas7(IStrategy):
                 (dataframe["close"] < (dataframe[f"ma_sell_{self.base_nb_candles_sell.value}"] * self.high_offset.value))
             ),
             ["buy", "buy_tag"]] = (1, "ewolow")
-
         dont_buy_conditions = []
-
         dont_buy_conditions.append(
             (
                 (dataframe["close_1h"].rolling(24).max() < (dataframe["close"] * self.min_profit.value ))
             )
         )
-
         if dont_buy_conditions:
             for condition in dont_buy_conditions:
                 dataframe.loc[condition, "buy"] = 0
-
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
