7ba1097036f4ee2dba9b12353a48ec9c03b7c17f
Abbas Elmas <abbaselmas@gmail.com>
2023-11-09 19:08:40 +0300

    BB imlement steps

--- user_data/strategies/abbas8.py
+++ user_data/strategies/abbas8.py
@@ -15,7 +15,6 @@ import technical.indicators as ftt
 from freqtrade.exchange import timeframe_to_prev_date
 from freqtrade.optimize.space import Categorical, Dimension, Integer, SKDecimal, Real
 
-
 # Protection hyperspace params:
 protection_params = {
     "cooldown_stop_duration_candles": 1,
@@ -31,7 +30,6 @@ protection_params = {
     "stoplossguard_stop_duration_candles": 20,
     "stoplossguard_trade_limit": 21
 }
-
 # Buy hyperspace params:
 buy_params = {
     "base_nb_candles_buy": 24,
@@ -46,7 +44,6 @@ buy_params = {
     "rsi_fast_ewo1": 35,
     "rsi_ewo2": 25
 }
-
 # Sell hyperspace params:
 sell_params = {
     "base_nb_candles_sell": 31,
@@ -59,7 +56,6 @@ sell_params = {
 class abbas8(IStrategy):
     def version(self) -> str:
         return "v8.5"
-
     INTERFACE_VERSION = 3
 
     cooldown_stop_duration_candles = IntParameter(0, 10, default=protection_params["cooldown_stop_duration_candles"], space="protection", optimize=True)
@@ -143,15 +139,13 @@ class abbas8(IStrategy):
                 Integer(2, 7, name='max_open_trades'),
             ]
 
+    timeframe = "5m"
+    inf_1h = "1h"
     max_open_trades = 3
-
-    # ROI table:
     minimal_roi = {
         "215": 0
     }
-
     stoploss = -0.084
-
     trailing_stop = True
     trailing_stop_positive = 0.0001
     trailing_stop_positive_offset = 0.0231
@@ -159,9 +153,12 @@ class abbas8(IStrategy):
 
     use_sell_signal = False
     ignore_roi_if_buy_signal = False
+    process_only_new_candles = True
+    startup_candle_count = 200
 
     max_change_pump = IntParameter(10, 70, default=sell_params["max_change_pump"], space="sell", optimize=True)
     pump_rolling = IntParameter(2, 70, default=sell_params["pump_rolling"], space="sell", optimize=True)
+    min_profit = DecimalParameter(0.10, 2.00, default=sell_params["min_profit"], space="sell", decimals=2, optimize=True)
 
     rsi_fast_ewo1 = IntParameter(30, 50, default=buy_params["rsi_fast_ewo1"], space="buy", optimize=True)
     rsi_ewo2 = IntParameter(14, 30, default=buy_params["rsi_ewo2"], space="buy", optimize=True)
@@ -183,21 +180,11 @@ class abbas8(IStrategy):
     ewo_high_2 = DecimalParameter(-2.0, 4.0, default=buy_params["ewo_high_2"], space="buy", decimals=2, optimize=protection_optimize)
     rsi_buy = IntParameter(50, 80, default=buy_params["rsi_buy"], space="buy", optimize=protection_optimize)
 
-    min_profit = DecimalParameter(0.10, 2.00, default=sell_params["min_profit"], space="sell", decimals=2, optimize=True)
-
     # Optional order time in force.
     order_time_in_force = {
         "entry": "gtc",
         "exit": "ioc"
     }
-
-    # Optimal timeframe for the strategy
-    timeframe = "5m"
-    inf_1h = "1h"
-
-    process_only_new_candles = True
-    startup_candle_count = 200
-
     slippage_protection = {
         "retries": 3,
         "max_slippage": -0.002
@@ -206,9 +193,7 @@ class abbas8(IStrategy):
     buy_signals = {}
 
     def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float, rate: float, time_in_force: str, sell_reason: str, current_time: datetime, **kwargs) -> bool:
-
         dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-
         try:
             state = self.slippage_protection["__pair_retries"]
         except KeyError:
@@ -226,48 +211,48 @@ class abbas8(IStrategy):
 
     def informative_pairs(self):
         pairs = self.dp.current_whitelist()
-        # Assign tf to each pair so they can be downloaded and cached for strategy.
         informative_pairs = [(pair, "1h") for pair in pairs]
         return informative_pairs
 
     def informative_1h_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         assert self.dp, "DataProvider is required for multiple timeframes."
         informative_1h = self.dp.get_pair_dataframe(pair=metadata["pair"], timeframe=self.inf_1h)
-
-        # Elliot
         informative_1h["ewo"] = EWO(informative_1h, int(self.fast_ewo.value), int(self.slow_ewo.value))
-
-        # RSI
         informative_1h["rsi"] = ta.RSI(informative_1h, timeperiod=14)
         informative_1h["rsi_fast"] = ta.RSI(informative_1h, timeperiod=4)
         informative_1h["rsi_slow"] = ta.RSI(informative_1h, timeperiod=20)
 
+        # bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(informative_1h), window=21, stds=2.8)
+        # informative_1h["bb_upperband"] = bollinger["upper"]
+        # informative_1h["bb_middleband"] = bollinger["mid"]
+        # informative_1h["bb_lowerband"] = bollinger["lower"]
+
+        # informative_1h["bb_bottom_cross"] = qtpylib.crossed_below(informative_1h['low'], informative_1h['bb_lowerband']).astype('int')
+        # informative_1h["bb_top_cross"] = qtpylib.crossed_above(informative_1h['high'], informative_1h['bb_upperband']).astype('int')
+
         return informative_1h
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
         # Calculate all ma_buy values
         for val in self.base_nb_candles_buy.range:
             dataframe[f"ma_buy_{val}"] = ta.EMA(dataframe, timeperiod=val)
-
         # Calculate all ma_sell values
         for val in self.base_nb_candles_sell.range:
             dataframe[f"ma_sell_{val}"] = ta.EMA(dataframe, timeperiod=val)
 
-        # Elliot
         dataframe["ewo"] = EWO(dataframe, int(self.fast_ewo.value), int(self.slow_ewo.value))
-
-        #Pump
         dataframe['pump'] = pump_warning(dataframe, perc=self.max_change_pump.value)
-
-        # RSI
         dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
         dataframe["rsi_fast"] = ta.RSI(dataframe, timeperiod=4)
         dataframe["rsi_slow"] = ta.RSI(dataframe, timeperiod=20)
+        # dataframe["bb_upperband"] = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=21, stds=2.8)["upper"]
+        # dataframe["bb_middleband"] = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=21, stds=2.8)["mid"]
+        # dataframe["bb_lowerband"] = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=21, stds=2.8)["lower"]
+        # dataframe["bb_bottom_cross"] = qtpylib.crossed_below(dataframe['low'], dataframe['bb_lowerband']).astype('int')
+        # dataframe["bb_top_cross"] = qtpylib.crossed_above(dataframe['high'], dataframe['bb_upperband']).astype('int')
 
         informative_1h = self.informative_1h_indicators(dataframe, metadata)
         dataframe = merge_informative_pair(dataframe, informative_1h, self.timeframe, self.inf_1h, ffill=True)
-
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
@@ -275,42 +260,31 @@ class abbas8(IStrategy):
         dataframe.loc[
             (
                 (dataframe["rsi_fast"] < self.rsi_fast_ewo1.value) &
-                # (dataframe["rsi_fast"] < 35) &
                 (dataframe["close"] < (dataframe[f"ma_buy_{self.base_nb_candles_buy.value}"] * self.low_offset.value)) &
                 (dataframe["ewo"] > self.ewo_high.value) &
                 (dataframe["rsi"] < self.rsi_buy.value) &
-                (dataframe["volume"] > 0) &
                 (dataframe["close"] < (dataframe[f"ma_sell_{self.base_nb_candles_sell.value}"] * self.high_offset.value))
             ),
             ["buy", "buy_tag"]] = (1, "ewo1")
-
         dataframe.loc[
             (
                 (dataframe["rsi_fast"] < self.rsi_fast_ewo1.value) &
-                # (dataframe["rsi_fast"] < 35) &
                 (dataframe["close"] < (dataframe[f"ma_buy_{self.base_nb_candles_buy.value}"] * self.low_offset_2.value)) &
                 (dataframe["ewo"] > self.ewo_high_2.value) &
                 (dataframe["rsi"] < self.rsi_buy.value) &
-                (dataframe["volume"] > 0) &
                 (dataframe["close"] < (dataframe[f"ma_sell_{self.base_nb_candles_sell.value}"] * self.high_offset.value)) &
-                # (dataframe["rsi"] < 25)
                 (dataframe["rsi"] < self.rsi_ewo2.value)
             ),
             ["buy", "buy_tag"]] = (1, "ewo2")
-
         dataframe.loc[
             (
                 (dataframe["rsi_fast"] < self.rsi_fast_ewo1.value) &
-                # (dataframe["rsi_fast"] < 35) &
                 (dataframe["close"] < (dataframe[f"ma_buy_{self.base_nb_candles_buy.value}"] * self.low_offset.value)) &
                 (dataframe["ewo"] < self.ewo_low.value) &
-                (dataframe["volume"] > 0) &
                 (dataframe["close"] < (dataframe[f"ma_sell_{self.base_nb_candles_sell.value}"] * self.high_offset.value))
             ),
             ["buy", "buy_tag"]] = (1, "ewolow")
-
         dont_buy_conditions = []
-
         dont_buy_conditions.append(
             (
                 (dataframe["close_1h"].rolling(24).max() < (dataframe["close"] * self.min_profit.value ))
@@ -321,11 +295,9 @@ class abbas8(IStrategy):
                 (dataframe['pump'].rolling(self.pump_rolling.value).max() < 1)
             )
         )
-
         if dont_buy_conditions:
             for condition in dont_buy_conditions:
                 dataframe.loc[condition, "buy"] = 0
-
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
