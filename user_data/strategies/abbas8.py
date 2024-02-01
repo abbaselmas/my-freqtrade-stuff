5f139a2f710a74a57e0833093d3520cb366ca1b8
Abbas Elmas <abbaselmas@gmail.com>
2024-02-01 15:02:32 +0300

    nfi volume protection try

--- user_data/strategies/abbas8.py
+++ user_data/strategies/abbas8.py
@@ -49,7 +49,12 @@ sell_params = {
     "base_nb_candles_sell": 21,
     "high_offset": 1.01,
     "volume_warn": 5.0,
-    "btc_rsi_8_1h": 35
+    "btc_rsi_8_1h": 35,
+    "percent_change_length": 60,
+    "hl_pct_change_06_1h": 0.80,
+    "hl_pct_change_12_1h": 0.90,
+    "hl_pct_change_24_1h": 0.95,
+    "hl_pct_change_48_1h": 1.00
 }
 
 class abbas8(IStrategy):
@@ -122,6 +127,8 @@ class abbas8(IStrategy):
             return roi_table
 
     timeframe = "5m"
+    info_timeframes = ["15m", "30m", "1h"]
+
     minimal_roi = {
         "106": 0,
         "189": -0.02,
@@ -142,7 +149,7 @@ class abbas8(IStrategy):
     rsi_fast_ewo1 = IntParameter(20, 60, default=buy_params["rsi_fast_ewo1"], space="buy", optimize=True)
     rsi_ewo2 = IntParameter(10, 40, default=buy_params["rsi_ewo2"], space="buy", optimize=True)
 
-    smaoffset_optimize = True
+    smaoffset_optimize = False
     base_nb_candles_buy = IntParameter(10, 50, default=buy_params["base_nb_candles_buy"], space="buy", optimize=smaoffset_optimize)
     base_nb_candles_sell = IntParameter(10, 50, default=sell_params["base_nb_candles_sell"], space="sell", optimize=smaoffset_optimize)
     low_offset = DecimalParameter(0.8, 1.2, default=buy_params["low_offset"], space="buy", decimals=2, optimize=smaoffset_optimize)
@@ -159,9 +166,16 @@ class abbas8(IStrategy):
     ewo_high_2 = DecimalParameter(-6.0, 12.0, default=buy_params["ewo_high_2"], space="buy", decimals=2, optimize=protection_optimize)
     rsi_buy = IntParameter(50, 85, default=buy_params["rsi_buy"], space="buy", optimize=protection_optimize)
 
-    dontbuy_optimize = True
+    dontbuy_optimize = False
     volume_warn = DecimalParameter(0.0, 10.0, default=sell_params["volume_warn"], space="sell", decimals=2, optimize=dontbuy_optimize)
     btc_rsi_8_1h = IntParameter(0, 50, default=sell_params["btc_rsi_8_1h"], space="sell", optimize=dontbuy_optimize)
+    percent_change_length = IntParameter(5, 288, default=sell_params["percent_change_length"], space="sell", optimize=dontbuy_optimize)
+
+    pct_chage_optimize = True
+    hl_pct_change_06_1h = DecimalParameter(00.30, 0.90, default=sell_params["hl_pct_change_06_1h"], decimals=2, space="sell", optimize=pct_chage_optimize)
+    hl_pct_change_12_1h = DecimalParameter(00.40, 1.00, default=sell_params["hl_pct_change_12_1h"], decimals=2, space="sell", optimize=pct_chage_optimize)
+    hl_pct_change_24_1h = DecimalParameter(00.50, 1.20, default=sell_params["hl_pct_change_24_1h"], decimals=2, space="sell", optimize=pct_chage_optimize)
+    hl_pct_change_48_1h = DecimalParameter(00.60, 1.60, default=sell_params["hl_pct_change_48_1h"], decimals=2, space="sell", optimize=pct_chage_optimize)
 
     # Optional order time in force.
     order_time_in_force = {
@@ -173,21 +187,70 @@ class abbas8(IStrategy):
         "max_slippage": -0.002
     }
 
-    # # Define informative upper timeframe for each pair. Decorators can be stacked on same
-    # # method. Available in populate_indicators as 'rsi_30m' and 'rsi_1h'.
-    # @informative('1h')
-    # def populate_indicators_1h(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-    #     dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
-    #     return dataframe
+    def informative_pairs(self):
+        pairs = self.dp.current_whitelist()
 
-    # Define BTC/STAKE informative pair. Available in populate_indicators and other methods as
-    # 'btc_rsi_1h'. Current stake currency should be specified as {stake} format variable
-    # instead of hard-coding actual stake currency. Available in populate_indicators and other
-    # methods as 'btc_usdt_rsi_1h' (when stake currency is USDT).
-    # @informative('1h', 'BTC/USDT')
-    # def populate_indicators_btc_1h(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-    #     dataframe['rsi_8'] = ta.RSI(dataframe, timeperiod=8)
-    #     return dataframe
+        informative_pairs = []
+        for info_timeframe in self.info_timeframes:
+            informative_pairs.extend([(pair, info_timeframe) for pair in pairs])
+
+        return informative_pairs
+
+    def informative_1h_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        assert self.dp, "DataProvider is required for multiple timeframes."
+        informative_1h = self.dp.get_pair_dataframe(pair=metadata["pair"], timeframe=self.inf_1h)
+
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(informative_1h), window=20, stds=2.68)
+        informative_1h["bb20_2_low"] = bollinger["lower"]
+        informative_1h["bb20_2_mid"] = bollinger["mid"]
+        informative_1h["bb20_2_upp"] = bollinger["upper"]
+
+        # Pump protections
+        informative_1h["hl_pct_change_48"] = range_percent_change(self, informative_1h, "HL", 48)
+        informative_1h["hl_pct_change_36"] = range_percent_change(self, informative_1h, "HL", 36)
+        informative_1h["hl_pct_change_24"] = range_percent_change(self, informative_1h, "HL", 24)
+        informative_1h["hl_pct_change_12"] = range_percent_change(self, informative_1h, "HL", 12)
+        informative_1h["hl_pct_change_6"]  = range_percent_change(self, informative_1h, "HL", 6)
+        
+        return informative_1h
+
+    def informative_30m_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        assert self.dp, "DataProvider is required for multiple timeframes."
+        informative_30m = self.dp.get_pair_dataframe(pair=metadata["pair"], timeframe=self.inf_30m)
+
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(informative_30m), window=20, stds=2.68)
+        informative_30m["bb20_2_low"] = bollinger["lower"]
+        informative_30m["bb20_2_mid"] = bollinger["mid"]
+        informative_30m["bb20_2_upp"] = bollinger["upper"]
+        
+        return informative_30m
+    
+    def informative_15m_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        assert self.dp, "DataProvider is required for multiple timeframes."
+        informative_15m = self.dp.get_pair_dataframe(pair=metadata["pair"], timeframe=self.inf_15m)
+
+        # BB - 20 STD2
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(informative_15m), window=20, stds=2.68)
+        informative_15m["bb20_2_low"] = bollinger["lower"]
+        informative_15m["bb20_2_mid"] = bollinger["mid"]
+        informative_15m["bb20_2_upp"] = bollinger["upper"]
+        
+        return informative_15m
+
+    def base_tf_5m_indicators(self, metadata: dict, dataframe: DataFrame) -> DataFrame:
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2.68)
+        dataframe["bb20_2_low"] = bollinger["lower"]
+        dataframe["bb20_2_mid"] = bollinger["mid"]
+        dataframe["bb20_2_upp"] = bollinger["upper"]
+
+        dataframe[f"ma_buy_{self.base_nb_candles_buy.value}"] = ta.EMA(dataframe, timeperiod=int(self.base_nb_candles_buy.value))
+        dataframe[f"ma_sell_{self.base_nb_candles_sell.value}"] = ta.EMA(dataframe, timeperiod=int(self.base_nb_candles_sell.value))
+        dataframe["ewo"] = EWO(dataframe, int(self.fast_ewo.value), int(self.slow_ewo.value))
+        dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
+        dataframe["rsi_fast"] = ta.RSI(dataframe, timeperiod=4)
+        dataframe["rsi_slow"] = ta.RSI(dataframe, timeperiod=20)
+
+        return dataframe
 
     def pump_dump_protection(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         df36h = dataframe.copy().shift( 432 )
@@ -214,13 +277,15 @@ class abbas8(IStrategy):
         return True
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        
-        dataframe[f"ma_buy_{self.base_nb_candles_buy.value}"] = ta.EMA(dataframe, timeperiod=int(self.base_nb_candles_buy.value))
-        dataframe[f"ma_sell_{self.base_nb_candles_sell.value}"] = ta.EMA(dataframe, timeperiod=int(self.base_nb_candles_sell.value))
-        dataframe["ewo"] = EWO(dataframe, int(self.fast_ewo.value), int(self.slow_ewo.value))
-        dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
-        dataframe["rsi_fast"] = ta.RSI(dataframe, timeperiod=4)
-        dataframe["rsi_slow"] = ta.RSI(dataframe, timeperiod=20)
+        dataframe       = self.base_tf_5m_indicators(metadata, dataframe)
+
+        informative_1h  = self.informative_1h_indicators(dataframe, metadata)
+        dataframe       = merge_informative_pair(dataframe, informative_1h, self.timeframe, self.inf_1h, ffill=True)
+        informative_30m = self.informative_30m_indicators(dataframe, metadata)
+        dataframe       = merge_informative_pair(dataframe, informative_30m, self.timeframe, self.inf_30m, ffill=True)
+        informative_15m = self.informative_15m_indicators(dataframe, metadata)
+        dataframe       = merge_informative_pair(dataframe, informative_15m, self.timeframe, self.inf_15m, ffill=True)
+
         dataframe = self.pump_dump_protection(dataframe, metadata)
         return dataframe
 
@@ -255,18 +320,35 @@ class abbas8(IStrategy):
             ["enter_long", "enter_tag"]] = (1, "ewolow")
 
         dont_buy_conditions = []
-        # don't buy if there seems to be a Pump and Dump event.
-        dont_buy_conditions.append(
-            (
-                (dataframe['pnd_volume_warn'] < 0.0)
-            )
-        )
+        # # don't buy if there seems to be a Pump and Dump event.
+        # dont_buy_conditions.append(
+        #     (
+        #         (dataframe['pnd_volume_warn'] < 0.0)
+        #     )
+        # )
         # # BTC price protection
         # dont_buy_conditions.append(
         #     (
         #         (dataframe['btc_usdt_rsi_8_1h'] < self.btc_rsi_8_1h.value)
         #     )
         # )
+        # # don't buy if the price has changed too much in the last *5 hours
+        # dont_buy_conditions.append(
+        #     (
+        #         ((dataframe['open'].rolling(self.percent_change_length.value).max() - dataframe['close']) / dataframe['close'])
+        #     )
+        # )
+        # pump protections
+        dont_buy_conditions.append(
+            (
+                (dataframe['hl_pct_change_48'] > self.hl_pct_change_48_1h.value) &
+                (dataframe['hl_pct_change_36'] > self.hl_pct_change_36_1h.value) &
+                (dataframe['hl_pct_change_24'] > self.hl_pct_change_24_1h.value) &
+                (dataframe['hl_pct_change_12'] > self.hl_pct_change_12_1h.value) &
+                (dataframe['hl_pct_change_06'] > self.hl_pct_change_06_1h.value)
+            )
+        )
+
         if dont_buy_conditions:
             for condition in dont_buy_conditions:
                 dataframe.loc[condition, "enter_long"] = 0
@@ -279,3 +361,34 @@ def EWO(dataframe, ema_length=5, ema2_length=35):
     ema1 = ta.EMA(dataframe, timeperiod=ema_length)
     ema2 = ta.EMA(dataframe, timeperiod=ema2_length)
     return (ema1 - ema2) / dataframe["low"] * 100
+
+def VWAPB(dataframe, window_size=20, num_of_std=1):
+    df = dataframe.copy()
+    df['vwap'] = qtpylib.rolling_vwap(df, window=window_size)
+    rolling_std = df['vwap'].rolling(window=window_size).std()
+    df['vwap_low'] = df['vwap'] - (rolling_std * num_of_std)
+    df['vwap_high'] = df['vwap'] + (rolling_std * num_of_std)
+    return df['vwap_low'], df['vwap'], df['vwap_high']
+
+# Volume Weighted Moving Average
+def vwma(dataframe: DataFrame, length: int = 10):
+    """Indicator: Volume Weighted Moving Average (VWMA)"""
+    # Calculate Result
+    pv = dataframe["close"] * dataframe["volume"]
+    vwma = Series(ta.SMA(pv, timeperiod=length) / ta.SMA(dataframe["volume"], timeperiod=length))
+    vwma = vwma.fillna(0, inplace=True)
+    return vwma
+
+def range_percent_change(self, dataframe: DataFrame, method, length: int) -> float:
+    if method == "HL":
+        return (dataframe["high"].rolling(length).max() - dataframe["low"].rolling(length).min()) / dataframe["low"].rolling(length).min()
+    elif method == "OC":
+        return (dataframe["open"].rolling(length).max() - dataframe["close"].rolling(length).min()) / dataframe["close"].rolling(length).min()
+    else:
+        raise ValueError(f"Method {method} not defined!")
+
+def top_percent_change(self, dataframe: DataFrame, length: int) -> float:
+    if length == 0:
+        return (dataframe["open"] - dataframe["close"]) / dataframe["close"]
+    else:
+        return (dataframe["open"].rolling(length).max() - dataframe["close"]) / dataframe["close"]
